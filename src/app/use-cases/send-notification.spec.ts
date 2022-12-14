0dcc8a4a6339cdf1f01d2aac51b02a96e379fa88
Diogo Santos <diogodeese@gmail.com>
2022-12-14 06:52:40 +0000

    test: in memory notifications repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,8 +1,10 @@
 import { SendNotification } from './send-notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       recipientId: 'example-recipient-id',
@@ -10,6 +12,7 @@ describe('Send notification', () => {
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
