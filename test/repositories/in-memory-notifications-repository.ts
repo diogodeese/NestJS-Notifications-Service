6f66636969b0e29458c97c0bd5b62c0a278a9b19
Diogo Santos <diogodeese@gmail.com>
2022-12-14 22:56:50 +0000

    paths

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,5 +1,5 @@
-import { Notification } from '../../src/app/entities/notification';
-import { NotificationsRepository } from '../../src/app/repositories/notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
