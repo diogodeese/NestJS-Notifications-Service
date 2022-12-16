3aa6fcd97e63bbb144691de88ef045803946c26d
Diogo Santos <diogodeese@gmail.com>
2022-12-16 05:07:55 +0000

    feat: prisma repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 import { Notification } from '@application/entities/notification';
+import { notification as RawNotification } from '@prisma/client';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        recipientId: raw.recipientId,
+        content: new Content(raw.content),
+        category: raw.category,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+        canceledAt: raw.canceledAt,
+      },
+      raw.id
+    );
   }
 }
